aac592f0c1e1f2c20ab013dfd34658ceb0edaa42
bhonnef <bhonnef>
2019-10-04 20:40:49 +0200

    text processing is now working, need to process the wikipedia text

---
+++ new/test.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import subprocess
+import re
+import os
+# from keras.utils import get_file
+
+
+def download_articles(limit=None):
+  base_url = 'https://dumps.wikimedia.org/enwiki/'
+  index = requests.get(base_url).text
+  soup_index = BeautifulSoup(index, 'html.parser')  # Find the links on the page
+  dumps = [a['href'] for a in soup_index.find_all('a') if a.has_attr('href')]
+
+  dump_url = base_url + dumps[-1]
+  dump_html = requests.get(dump_url).text
+
+  # Convert to a soup
+  soup_dump = BeautifulSoup(dump_html, 'html.parser')
+
+  file_folder = "C:/projects/information-extraction/new/data/"
+  # Downloading only the articles
+  for link in soup_dump.find_all(
+      'a', {'href': re.compile(r".*pages-articles\d\.xml-p.*\.bz2$")},
+      limit=limit):
+    path = file_folder + link["href"]
+    if not os.path.exists(path):
+      print("Downloading: " + link["href"])
+      get_file(path, dump_url + link["href"])
+      print("File size: " + str(os.stat(path).st_size / 1e6) + " MB")
+
+
+# file = "./data/enwiki-latest-pages-articles15.xml-p7744803p9244803.bz2"
+
+# for line in subprocess.Popen(["bzcat"], stdin=open(file), stdout=subprocess.PIPE).stdout:
+#   print(line)
+
+# download_articles()
+
+import stanfordnlp
+
+stanfordnlp.download("en", resource_dir="./stanfordnlp_resources")
+nlp = stanfordnlp.Pipeline(models_dir="./stanfordnlp_resources/")
+doc = nlp("I just wanted an ice-cream. Because she likes it.")
+doc.sentences[0].print_dependencies()
